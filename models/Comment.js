5672903b9948c6213a058644abcc225edb90893c
Louis Dalligos <zindex14@gmail.com>
2018-02-22 20:06:24 +0800

    Complete our api skeleton for our 2 models

--- models/Comment.js
+++ models/Comment.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 
 var CommentSchema = new mongoose.Schema({
 
-  username: { type: String, default: '' }
+  username: { type: String, default: '' },
   body: { type: String, default: '' },
   timestamp: { type: Date, default: Date.now }
 
--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,37 @@
 var express = require('express')
 var router = express.Router()
 var ZoneController = require('../controllers/ZoneController')
+var controllers = require('../controllers')
 
 router.get('/:resource', function(req, res, next) {
 
   var resource = req.params.resource
+  var controller = controllers[resource]
 
-  if ( resource = 'zone' ) {
-    ZoneController.find(req.query, function(err, results) {
-      if (err) {
-        res.json({
-          confirmation: 'fail',
-          message: err
-        })
-
-        return
-      }
-
-      res.json({
-        confirmation: 'success',
-        results: results
-      })
+  if ( controller == null ) {
+    res.json({
+      confirmation: 'fail',
+      message: 'Resource not found: ' + resource
     })
+
+    return
   }
+
+  controller.find(req.query, function(err, results) {
+    if (err) {
+      res.json({
+        confirmation: 'fail',
+        message: err
+      })
+
+      return
+    }
+
+    res.json({
+      confirmation: 'success',
+      results: results
+    })
+  })
 
 })
 
@@ -30,48 +39,66 @@ router.get('/:resource/:id', function(req, res, next) {
 
   var resource = req.params.resource
   var id = req.params.id
+  var controller = controllers[resource]
 
-  if ( resource == 'zone' ) {
-    ZoneController.findById(id, function(err, result) {
-      if (err) {
-        res.json({
-          confirmation: 'fail',
-          message: 'Not Found'
-        })
-
-        return
-      }
-
-      res.json({
-        confirmation: 'success',
-        result: result
-      })
+  if ( controller == null ) {
+    res.json({
+      confirmation: 'fail',
+      message: 'Resource not found: ' + resource
     })
+
+    return
   }
+
+  controller.findById(id, function(err, result) {
+    if (err) {
+      res.json({
+        confirmation: 'fail',
+        message: 'Not Found'
+      })
+
+      return
+    }
+
+    res.json({
+      confirmation: 'success',
+      result: result
+    })
+  })
+  
 
 })
 
 router.post('/:resource', function(req, res, next) {
 
   var resource = req.params.resource
-
-  if ( resource == 'zone' ) {
-    ZoneController.create(req.body, function(err, result) {
-      if (err) {
-        res.json({
-          confirmation: 'fail',
-          message: err
-        })
-
-        return
-      }
-
-      res.json({
-        confirmation: 'success',
-        result: result
-      })
+  var controller = controllers[resource]
+  
+  if ( controller == null ) {
+    res.json({
+      confirmation: 'fail',
+      message: 'Resource not found: ' + resource
     })
+
+    return
   }
+
+  controller.create(req.body, function(err, result) {
+    if (err) {
+      res.json({
+        confirmation: 'fail',
+        message: err
+      })
+
+      return
+    }
+
+    res.json({
+      confirmation: 'success',
+      result: result
+    })
+  })
+  
 })
 
 
