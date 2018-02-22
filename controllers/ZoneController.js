fa3940c6dd75b5995bf6db6cbca03cf66adf90f9
Louis Dalligos <zindex14@gmail.com>
2018-02-22 16:38:02 +0800

    Add our find, findbyId controllers

---
+++ controllers/ZoneController.js
@@ -0,0 +1,46 @@
+var Zone = require('../models/Zone')
+
+module.exports = {
+
+  find: function(params, callback) {
+    Zone.find(params, function(err, zones) {
+      if (err) {
+        callback(err, nul)
+        return
+      }
+
+      callback(null, zones)
+    })
+  },
+
+  findById: function(id, callback) {
+    Zone.findById(id, function(err, zone) {
+      if (err) {
+        callback(err, null)
+        return
+      }
+
+      callback(null, zone)
+    })
+  },
+
+  create: function(params, callback) {
+    Zone.create(params, function(err, zone) {
+      if (err) {
+        callback(err, null)
+        return
+      }
+
+      callback(null, zone)
+    })
+  },
+
+  update: function() {
+
+  },
+
+  destroy: function() {
+
+  }
+
+}
--- routes/api.js
+++ routes/api.js
@@ -6,10 +6,50 @@ router.get('/:resource', function(req, res, next) {
 
   var resource = req.params.resource
 
-  res.json({
-    confirmation: 'success',
-    resource: resource
-  })
+  if ( resource = 'zone' ) {
+    ZoneController.find(req.query, function(err, results) {
+      if (err) {
+        res.json({
+          confirmation: 'fail',
+          message: err
+        })
+
+        return
+      }
+
+      res.json({
+        confirmation: 'success',
+        results: results
+      })
+    })
+  }
+
+})
+
+
+router.get('/:resource/:id', function(req, res, next) {
+
+  var resource = req.params.resource
+  var id = req.params.id
+
+  if ( resource == 'zone' ) {
+    ZoneController.findById(id, function(err, result) {
+      if (err) {
+        res.json({
+          confirmation: 'fail',
+          message: 'Not Found'
+        })
+
+        return
+      }
+
+      res.json({
+        confirmation: 'success',
+        result: result
+      })
+    })
+  }
+
 })
 
 module.exports = router
