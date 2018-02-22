b04838ff545dd907eacd9f7481d38631ec464364
Louis Dalligos <zindex14@gmail.com>
2018-02-22 17:21:06 +0800

    Split zipcodes to array

--- controllers/ZoneController.js
+++ controllers/ZoneController.js
@@ -25,6 +25,15 @@ module.exports = {
   },
 
   create: function(params, callback) {
+    var zips = params['zipCodes']
+    var zip = zips.split(',')
+    var newZips = []
+    zip.forEach( function(zipCode) {
+      newZips.push(zipCode.trim())
+    })
+
+    params['zipCodes'] = newZips
+
     Zone.create(params, function(err, zone) {
       if (err) {
         callback(err, null)
