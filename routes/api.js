75016eaa8ae0c8da52d9c576fe41bef6303cee7a
Louis Dalligos <zindex14@gmail.com>
2018-02-22 11:15:31 +0800

    Scaffold our project

---
+++ routes/api.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var router = express.Router()
+var ZoneController = require('../controllers/ZoneController')
+
+router.get('/:resource', function(req, res, next) {
+
+  var resource = req.params.resource
+
+  res.json({
+    confirmation: 'success',
+    resource: resource
+  })
+})
+
+module.exports = router
